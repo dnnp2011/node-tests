c5d75bb4ff40a1b5fccdbbc885c7ef3b3b37940f
Dalton Pierce <orchardblockchain@gmail.com>
2018-10-30 11:50:51 -0400

    Added frame-print package - Added npm-packages lession

---
+++ bin/npm-packages.js
@@ -0,0 +1,12 @@
+// Import my NPM package
+const framePrint = require('../frame-print');
+
+framePrint('Welcome to NPM!');
+
+/*
+    Expected Output:
+
+    **********
+    Hello NPM!
+    **********
+ */
